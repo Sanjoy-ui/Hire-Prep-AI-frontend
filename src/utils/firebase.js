16f4a5c455ace38fee52f6bad33c7f57eb05b239
Sanjoy Deb <sanjoydeb404@gmail.com>
2026-04-15 01:49:39 +0530

    node version fix for deployment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -22,7 +22,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
-// import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
